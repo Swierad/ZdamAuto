b84f985019bdea18f8902f51d4ab627054e9e530
Józef Śliwiński <58230206+Swierad@users.noreply.github.com>
2020-09-06 15:36:20 +0200

    Update settings.py

--- AutoZdam/AutoZdam/settings.py
+++ AutoZdam/AutoZdam/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, "static"),
 #    '/var/www/static/',
-]
+#]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
