d36d620d0d12148f977ec9a8e460181646ac8f00
Józef Śliwiński <58230206+Swierad@users.noreply.github.com>
2020-09-06 13:58:37 +0200

    Update settings.py

--- AutoZdam/AutoZdam/settings.py
+++ AutoZdam/AutoZdam/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#    '/var/www/static/',
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
